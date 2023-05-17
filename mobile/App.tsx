5dd83678cfedda5d20c413ff1002b13593d2c78e
valvanogabryel <valvanogabryel@gmail.com>
2023-05-17 15:57:08 -0300

    ;

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ImageBackground, Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+
+import {
+  BaiJamjuree_700Bold
+} from '@expo-google-fonts/bai-jamjuree';
+
+import bgBlur from './src/assets/bg-blur.png';
+import Stripes from './src/assets/stripes.svg';
+import { styled } from 'nativewind';
+
+const StyledStripes = styled(Stripes);
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  });
+
+  if (!hasLoadedFonts) return null;
+
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className='text-zinc-50'>Hello, World!</Text>
+    <ImageBackground
+      className="flex-1 items-center relative bg-gray-900"
+      source={bgBlur}
+      imageStyle={{
+        position: 'absolute',
+        left: '-100%'
+      }}
+    >
+      <StyledStripes className='absolute left-2' />
+
+      <View className='flex-1 items-center justify-center gap-6'>
+
+      </View>
+
       <StatusBar style="auto" translucent />
-    </View>
+    </ImageBackground>
   );
 }
