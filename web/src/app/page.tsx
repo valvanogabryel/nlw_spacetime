fb73673ef36849e71b83753483c4db2a716675b1
valvanogabryel <valvanogabryel@gmail.com>
2023-05-17 12:21:23 -0300

    ;

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="h-screen text-zinc-50 bg-zinc-950 p-6">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+
+    </main>
   )
 }
