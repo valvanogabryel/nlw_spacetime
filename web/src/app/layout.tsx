ad97e62f22ac97b1d50d854bd1df07ed0f8e862c
valvanogabryel <valvanogabryel@gmail.com>
2023-05-17 10:37:38 -0300

    /

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,19 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree
+} from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  subsets: ['latin'],
+  variable: '--font-roboto',
+});
+
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+});
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +26,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans`}>{children}</body>
     </html>
   )
 }
