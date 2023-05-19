4ac3a6d47d75cba05810c180164eceb8089e3e16
valvanogabryel <valvanogabryel@gmail.com>
2023-05-18 22:15:37 -0300

    ;

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,24 @@
 import 'dotenv/config';
 import fastify from 'fastify';
 import cors from '@fastify/cors';
+import multipart from '@fastify/multipart';
 import jwt from '@fastify/jwt';
 import { memoriesRoutes } from './routes/memories';
 import { authRoutes } from './routes/auth';
+import { uploadRoutes } from './routes/upload';
+import { resolve } from 'node:path';
 
 const app = fastify();
+
+app
+  .register(multipart);
+
+app
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  .register(require('@fastify/static'), {
+    root: resolve(__dirname, '../uploads'),
+    prefix: '/uploads',
+  });
 
 app
   .register(jwt, {
@@ -22,7 +35,11 @@ app
   .register(authRoutes);
 
 app
+  .register(uploadRoutes);
+
+app
   .register(memoriesRoutes);
+
 
 app
   .listen({
